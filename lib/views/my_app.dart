3bdcff7205ab71700430aceb1d4667b90a8b8296
Nicos Nicolaou <nicosnicos16@gmail.com>
2023-04-22 00:55:02 +0300

    orientation screen only portrait

--- lib/views/my_app.dart
+++ lib/views/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sampledriftdatabasewithblocpattern/views/ships_screen/ships_screen.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,6 +7,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
